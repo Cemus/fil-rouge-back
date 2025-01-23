a2c55db13cdbe6f075a7616f71b56d5b487ce280
Cemus <24947582+Cemus@users.noreply.github.com>
2025-01-23 09:22:51 +0100

    Fix port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require("./routes/routes");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
